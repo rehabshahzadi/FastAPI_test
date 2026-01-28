adda3f32d479b350d06d4ff860d377f1924565c0
rehabshahzadi <rehabshahzadi@gmail.com>
2026-01-28 17:00:12 +0100

    Endpoint Delete

---
+++ cur_D_.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
+import json
+
+app = FastAPI()
+
+class Patient(BaseModel):
+
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+    
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+def save_data(data):
+    with open('patients.json', 'w') as f:
+        json.dump(data, f)
+
+
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    # load all the patients
+    data = load_data()
+    if patient_id in data:
+        del data[patient_id]
+        save_data(data)
+        return JSONResponse(status_code=200, content={'message':'Patient deleted successfully'})
+
+    raise HTTPException(status_code=404, detail='Patient not found')
